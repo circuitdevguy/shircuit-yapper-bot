53a55a31fc9b2594e9e2e1d43886a73d53c5f1d8
circuitdevguy <circuitsthecoder@gmail.com>
2024-05-18 12:53:53 -0300

    Create raw-alt-br.js
    brazillian version of raw alt

---
+++ raw-alt-br.js
@@ -0,0 +1,14 @@
+MPP.client.on("a", function (msg) {
+// define base thingies
+var args = msg.a.split(" ");
+var cmd = args[0].toLowerCase();
+args = args.slice(1);
+var sendChat = function(msg) { MPP.chat.send(msg) };
+var name = msg.p.name;
+// command things
+if (cmd === "yap.topic") {
+	if (msg.p.id === "6420abac45caf6db5876e564" || msg.p.id === "ebb6dfa96f1714ce66f648ad") {
+		sendChat("gente vamos falar do número " + Math.floor(Math.random()*100));
+	}
+}
+});
