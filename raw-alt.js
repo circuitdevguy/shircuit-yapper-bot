2949c3c4bdf621ae8e61e5b7dfea3e06f5973762
circuitdevguy <circuitsthecoder@gmail.com>
2024-05-19 00:45:21 -0300

    Update raw-alt.js
    changed admin permission to public and made the code a bit evil-er >:)

--- raw-alt.js
+++ raw-alt.js
@@ -9,8 +9,6 @@ var sendChat = function(msg) { MPP.chat.send(msg) };
 var name = msg.p.name;
 // command things
 if (cmd === "yap.topic") {
-	if (msg.p.id === "6420abac45caf6db5876e564" || msg.p.id === "ebb6dfa96f1714ce66f648ad") {
-		sendChat("guys let's talk about the number " + Math.floor(Math.random()*100));
-	}
+	sendChat("guys let's talk about the number " + Math.floor(Math.random()*10000));
 }
 });
