ac26fa7f5d3f22f7af9897f0c7e1e1d29386bca1
circuitdevguy <circuitsthecoder@gmail.com>
2024-05-19 21:52:26 -0300

    Update raw-alt-br.js
    now is translated version of raw-alt

--- raw-alt-br.js
+++ raw-alt-br.js
@@ -1,5 +1,6 @@
-// just a "show to the brazillian users" version
-
+var devs = [
+	"6420abac45caf6db5876e564"
+]
 MPP.client.on("a", function (msg) {
 // define base thingies
 var args = msg.a.split(" ");
@@ -7,10 +8,23 @@ var cmd = args[0].toLowerCase();
 args = args.slice(1);
 var sendChat = function(msg) { MPP.chat.send(msg) };
 var name = msg.p.name;
-// command things
+// make new topic to yap about
 if (cmd === "yap.topic") {
 	if (msg.p.id === "6420abac45caf6db5876e564" || msg.p.id === "ebb6dfa96f1714ce66f648ad") {
-		sendChat("gente vamos falar do número " + Math.floor(Math.random()*100));
+		sendChat("agora, vamos falar sobre " + Math.floor(Math.random()*100));
 	}
+}
+if (cmd === "yap.amidev") {
+	if (devs.indexOf(msg.p.id) == -1) {
+		sendChat("Você... (bateria) ||NÃO é um desenvolvedor!||")
+	} else {
+		sendChat("Você... (bateria) ||É um desenvolvedor!||")
+	}
+}
+if (cmd === "yap.source") {
+	sendChat("Siga meu desenvolvimento em https://github.com/circuitdevguy/shircuit-yapper-bot!")
+}
+if (cmd === "yap.help") {
+	sendChat("Os comandos faladores do falador! yap.source, yap.amidev, yap.topic") // update with each new command
 }
 });
