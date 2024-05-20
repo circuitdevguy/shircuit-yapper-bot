dd2ce26a6ebe3d8d8e1c40bfee170d9a47a8623c
circuitdevguy <circuitsthecoder@gmail.com>
2024-05-19 21:41:44 -0300

    Update raw-alt.js
    updated to newest verified version

--- raw-alt.js
+++ raw-alt.js
@@ -1,5 +1,3 @@
-// use when bugfixing
-
 MPP.client.on("a", function (msg) {
 // define base thingies
 var args = msg.a.split(" ");
@@ -7,8 +5,17 @@ var cmd = args[0].toLowerCase();
 args = args.slice(1);
 var sendChat = function(msg) { MPP.chat.send(msg) };
 var name = msg.p.name;
-// command things
+// make new topic to yap about
 if (cmd === "yap.topic") {
-	sendChat("guys let's talk about the number " + Math.floor(Math.random()*10000));
+	if (msg.p.id === "6420abac45caf6db5876e564" || msg.p.id === "ebb6dfa96f1714ce66f648ad") {
+		sendChat("guys let's talk about the number " + Math.floor(Math.random()*100));
+	}
+}
+if (cmd === "yap.amidev") {
+	if (devs.indexOf(msg.p.id) == -1) {
+		sendChat("You're... (drumroll) ||NOT a yapper dev! \*crowd gasp*||")
+	} else {
+		sendChat("You're... (drumroll) ||YES a yapper dev! \*crowd gasp*||")
+	}
 }
 });
